17fa16a4f607cbe460fa644ef783ce7027af4e3f
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2017-01-29 19:38:50 -0300

    Export combineStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Reduxion from './Reduxion'
 import Reducer from './Reducer'
 import createStore from './createStore'
+import combineStore from './combineStore'
 
 export default Reduxion
 export {
   createStore,
+  combineStore,
   Reducer,
 }

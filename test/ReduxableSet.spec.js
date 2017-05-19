43dc83c25e02dd887dbaaa1c0a3abd5581fb27ca
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2017-05-19 19:02:01 -0300

    Fix issue with nested ReduxableSet

--- test/ReduxableSet.spec.js
+++ test/ReduxableSet.spec.js
@@ -96,6 +96,56 @@ describe('Reduxable', () => {
       store.dispatch({ type: 'INCREMENT' })
       expect(reduxableSet.getState()).toEqual({ counterOne: 1, counterTwo: 1 })
     })
+
+    it('should support nested ReduxableSet', () => {
+      const reduxableSet = new ReduxableSet({
+        oneChild: new ReduxableSet({
+          anotherChild: new ReduxableSet({
+            counterOne: new Counter(),
+            counterTwo: new Counter(),
+          }),
+        }),
+      })
+
+      const store = createStore(reduxableSet)
+      Reduxable.setStore(store)
+      ReduxableSet.setStore(store)
+
+      expect(reduxableSet.getState()).toEqual({ oneChild: { anotherChild: { counterOne: 0, counterTwo: 0 } } })
+
+      reduxableSet.oneChild.anotherChild.counterOne.increment()
+      expect(reduxableSet.getState()).toEqual({ oneChild: { anotherChild: { counterOne: 1, counterTwo: 0 } } })
+
+      reduxableSet.oneChild.anotherChild.counterTwo.increment()
+      expect(reduxableSet.getState()).toEqual({ oneChild: { anotherChild: { counterOne: 1, counterTwo: 1 } } })
+    })
+
+    it('should support similar Reduxables nested at first level', () => {
+      const reduxableSet = new ReduxableSet({
+        childOne: new ReduxableSet({
+          counter: new Counter(),
+        }),
+        childTwo: new ReduxableSet({
+          counter: new Counter(),
+        }),
+      })
+
+      const store = createStore(reduxableSet)
+      Reduxable.setStore(store)
+      ReduxableSet.setStore(store)
+
+      expect(reduxableSet.childOne._scope).toEqual('childOne')
+      expect(reduxableSet.childTwo._scope).toEqual('childTwo')
+      expect(reduxableSet.childOne.counter._scope).toEqual('childOne.counter')
+
+      expect(reduxableSet.getState()).toEqual({ childOne: { counter: 0 }, childTwo: { counter: 0 } })
+
+      reduxableSet.childOne.counter.increment()
+      expect(reduxableSet.getState()).toEqual({ childOne: { counter: 1 }, childTwo: { counter: 0 } })
+
+      reduxableSet.childTwo.counter.increment()
+      expect(reduxableSet.getState()).toEqual({ childOne: { counter: 1 }, childTwo: { counter: 1 } })
+    })
   })
 
   describe('extending the ReduxableSet class', () => {
@@ -148,7 +198,7 @@ describe('Reduxable', () => {
     })
   })
 
-  describe('no connected with Redux', () => {
+  describe('no-connected with Redux', () => {
     it('should work even disconnected from Redux', () => {
       const reduxableSet = new ReduxableSet({
         counterOne: new Counter(),
